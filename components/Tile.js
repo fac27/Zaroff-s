5b0b7d14cc73e2e00ae6822d26a01ad133c3558a
FomasTreeman <tom@team-freeman.com>
2023-06-19 17:27:05 +0100

    initial

--- components/Tile.js
+++ components/Tile.js
@@ -1,5 +1,28 @@
-export default function Tile() {
-  <div>
-    <article className=""></article>
-  </div>;
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from 'next/image';
+import styles from './tile.module.css';
+
+export default function Tile(props) {
+  return (
+    <article className={[styles.flex, styles.spaceBetween]}>
+      <figure>
+        <Image
+          src={props.imageUrl}
+          alt={props.islandName}
+          width={200}
+          height={200}
+        />
+        <caption className={styles.caption}>
+          <b>{props.islandName}</b>
+          <b className={styles.thin}>{props.area} acres</b>
+        </caption>
+      </figure>
+      <div>
+        <small>{props.region}</small>
+        <small>GBP {props.price}</small>
+        <p> {props.description}</p>
+      </div>
+      {/* <FontAwesomeIcon icon={'fa-regular fa-coin-vertical'} /> */}
+    </article>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
+import Tile from '../components/Tile';
+import image from '../public/images/camera.example.jpg';
+
 export default function Home() {
-  return <h1> Hello World!</h1>;
+  return (
+    <>
+      <Tile
+        description={'akjsdnfkasd'}
+        area={123123}
+        price={123123}
+        region={'home'}
+        islandName={'my island'}
+        imageUrl={image}
+      />
+      <h1> Hello World!</h1>
+    </>
+  );
 }
