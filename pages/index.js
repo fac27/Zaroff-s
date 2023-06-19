1a71c6bb4b16c1923e640908f9ba93a343053415
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-06-19 17:32:08 +0100

    displaying basic home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
+import Layout from "../components/Layout.js";
+import Head from "next/head";
+
 export default function Home() {
-  return <h1> Hello World!</h1>;
+  return (
+    <Layout home>
+      <Head>
+        <title>Zaroff's Island Emporium</title>
+      </Head>
+      <section>
+        <h1>Image of an Island here</h1>
+      </section>
+      <section>
+        <h2>Island Tiles here</h2>
+      </section>
+    </Layout>
+  );
 }
