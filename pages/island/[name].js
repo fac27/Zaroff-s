f8f64db36d0c67173988b556220b1dab29053bf2
simonryrie <simonryrie@gmail.com>
2023-06-20 12:09:12 +0100

    Models working - dynamic paths not yet

--- pages/island/[name].js
+++ pages/island/[name].js
@@ -1,4 +1,6 @@
 import { getIslandData, getAllIslands } from "../../utils/islands.js";
+import Layout from "@/components/Layout.js";
+import Head from "next/head"
 
 // create paths for each existing island
 export async function getStaticPaths() {
@@ -10,11 +12,23 @@ export async function getStaticPaths() {
 }
 
 // create props for each island from it's id fed from static paths
-export async function getStaticProps({ params }) {
-  const islandData = await getIslandData(params.name);
+export async function getStaticProps({ params }) { // how does getStaticProps work? Where are params coming from?
+  console.log(params) // Only returning { name: 'sunset-haven' }
+  const islandData = await getIslandData(params.name); // No id to return
   return {
     props: {
       islandData,
     },
   };
+}
+
+export default function Island({islandData}){
+  return (
+    <Layout>
+      <Head>
+        <title>{islandData}</title>
+      </Head>
+      <h1>Hello Island</h1>
+    </Layout>
+  )
 }
--- utils/islands.js
+++ utils/islands.js
@@ -2,14 +2,15 @@ import { select_one_island, select_all_islands } from "./models/islands.js";
 
 export function getAllIslands() {
   const allIslands = select_all_islands.all();
-
-  return allIslands.map((island) => {
+  const islandsArray = allIslands.map((island) => {
     return {
       params: {
         id: island.id,
-      },
+        name: island.name.toLowerCase().replace(/\s/g,"-")
+      }
     };
   });
+  return islandsArray;
 }
 
 export default function getIslandData(id) {
