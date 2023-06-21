1fb439d687b18d56896f12ff7f25da9d818c91f8
hanleymark <home@markhanley.co.uk>
2023-06-21 10:42:53 +0100

    Banner bg image in place and resizes. Not sticky

--- components/Banner.js
+++ components/Banner.js
@@ -1,45 +1,53 @@
-import Image from "next/image";
-import styles from "./Banner.module.css";
-import Link from "next/link";
-import { useEffect } from "react";
+import Image from 'next/image';
+import styles from './Banner.module.css';
+import Link from 'next/link';
+import { useEffect, useRef } from 'react';
 
 export default function Banner() {
+  const bannerRef = useRef(null);
+  const titleRef = useRef(null);
+
   useEffect(() => {
     const handleScroll = () => {
-      //   if (document.body.scrollTop > 90 ||
-      //     document.documentElement.scrollTop > 90)
-      // {
-      //     document.getElementById("navlist")
-      //                 .style.padding = "25px 10px";
-      //     document.getElementById("logo")
-      //             .style.fontSize = "24px";
-      // }
-      // else {
-      //     document.getElementById("navlist")
-      //                 .style.padding = "90px 10px";
-      //     document.getElementById("logo")
-      //                 .style.fontSize = "35px";
-      // }
+      if (
+        document.body.scrollTop > 90 ||
+        document.documentElement.scrollTop > 90
+      ) {
+        bannerRef.current.style.height = '100px';
+        bannerRef.current.style.padding = '25px 10px';
+        titleRef.current.style.opacity = '0';
+      } else {
+        bannerRef.current.style.height = '500px';
+        bannerRef.current.style.padding = '90px 10px';
+        titleRef.current.style.opacity = '1';
+      }
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
     // Clean up function
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   return (
     <header>
-      <div className={styles.banner__title}>
-        <h1>Zaroff&apos;s Island Emporium</h1>
-        <h2>Because finding your island paradise shouldn&apos;t be atoll</h2>
-      </div>
-      <div className={styles.banner__nav}>
-        <Link href="/">home</Link>
-        <Link href="#islands">Our islands</Link>
+      <div className={styles.banner__outer} ref={bannerRef}>
+      <div className={styles.banner__nav}>Navigation</div>
+        <div className={styles.banner__title} ref={titleRef}>
+          <h1>Zaroff&apos;s Island Emporium</h1>
+          <h2>Because finding your island paradise shouldn&apos;t be atoll</h2>
+        </div>
+        
       </div>
     </header>
   );
+}
+
+{
+  /* <div className={styles.banner__nav}>
+  <Link href="/">home</Link>
+  <Link href="#islands">Our islands</Link>
+</div>; */
 }
