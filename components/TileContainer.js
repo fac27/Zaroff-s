f13be2fd93722b27efeae4e21c82246a0f3152b5
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-06-21 11:20:12 +0100

    raised state for filter

--- components/TileContainer.js
+++ components/TileContainer.js
@@ -2,11 +2,12 @@ import styles from "./TileContainer.module.css";
 import Tile from "./Tile";
 
 export default function TileContainer(props) {
-
   return (
     <main className={styles.main}>
       {props.islands
-        .filter((island) => island.region === props.filter || props.filter === "all")
+        .filter(
+          (island) => island.region === props.filter || props.filter === "all"
+        )
         .map((island, i) => (
           <Tile key={i} island={island} />
         ))}
