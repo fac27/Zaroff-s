8164dbc99001ad482d96f6bbc1a41fa50c117b07
FomasTreeman <tom@team-freeman.com>
2023-06-21 11:14:01 +0100

    yay

--- components/TileContainer.js
+++ components/TileContainer.js
@@ -2,6 +2,7 @@ import styles from './TileContainer.module.css';
 import Tile from './Tile';
 
 export default function TileContainer({ islands }) {
+  console.log('TileContainer', islands);
   return (
     <main className={styles.main}>
       {islands.map((island, i) => (
