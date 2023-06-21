25c4df7b15ac295deadeceef2a7d8654609b2902
FomasTreeman <tom@team-freeman.com>
2023-06-21 16:30:42 +0100

    params refactor

--- pages/basket.js
+++ pages/basket.js
@@ -7,17 +7,16 @@ import { commas } from "@/utils/format";
 import styles from "./basket.module.css";
 
 export function getStaticProps() {
-  const islands = getAllIslands().params;
+  const islands = getAllIslands();
   return {
     props: {
-      islands,
+      params: { islands },
     },
   };
 }
 
-export default function Basket({ islands }) {
+export default function Basket({ params: { islands } }) {
   const { basket } = useContext(BasketContext);
-
   if (!basket) return;
   const basketData = islands.filter(island => basket.includes(island.name));
   const total = basketData.reduce((acc, curr) => (acc += curr.price), 0);
