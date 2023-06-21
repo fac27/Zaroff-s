6a0b8ec805e0c65faff834b345686e12457f8598
FomasTreeman <tom@team-freeman.com>
2023-06-21 10:59:44 +0100

    viable basket

---
+++ components/BasketItem.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import styles from './basketItem.module.css';
+
+export default function Item({
+  islandData: { img_path: imagePath, name: islandName, region, price, area },
+}) {
+  return (
+    <div className={styles.container}>
+      <Image src={imagePath} alt={islandName} width={300} height={200} />
+      <b>{islandName}</b>
+      <small> {region}</small>
+      <p> Quantity: 2</p>
+      <p> £{price}</p>
+      {/* delete button */}
+      {/* <button onClick={}></button> */}
+    </div>
+  );
+}
--- pages/basket.js
+++ pages/basket.js
@@ -1,50 +1,36 @@
-import Link from "next/link";
-import Image from "next/image";
-import { useEffect, useContext, useState } from "react";
-import { BasketContext } from "@/context/context";
-import styles from "./basket.module.css";
+import Link from 'next/link';
+import { useEffect, useContext } from 'react';
+import { BasketContext } from '@/context/context';
+import BasketItem from '../components/BasketItem';
 
 export default function Basket() {
   const { basket, setBasket } = useContext(BasketContext);
-  const [total, setTotal] = useState(0);
-
-  useEffect(() => {
-    if (!basket.length) return;
-    setTotal(basket.reduce((acc, curr) => (acc += curr.price), 0));
-  }, [basket]);
 
   // load local storage if exists and set when leaving
+
   useEffect(() => {
-    const oldBasket = window.localStorage.getItem("basket");
-    if (oldBasket.length > 0) setBasket(JSON.parse(oldBasket));
-    return () => {
-      console.log("basket setting local storage");
-      window.localStorage.setItem("basket", JSON.stringify(basket));
-    };
-  }, []);
+    console.log('picking up old basket');
+    const oldBasket = window.localStorage.getItem('basket');
+    if (oldBasket) setBasket(JSON.parse(oldBasket));
+  }, [setBasket]);
+
+  if (!basket.length) return;
+
+  console.log('basket setting local storage', basket.length);
+  window.localStorage.setItem('basket', JSON.stringify(basket));
+
+  const total = basket.reduce((acc, curr) => (acc += curr.price), 0);
 
   return (
     <>
       <h1> Basket </h1>
       {basket &&
-        basket.map((islandData, i) => <Item key={i} islandData={islandData} />)}
+        basket.map((islandData, i) => (
+          <BasketItem key={i} islandData={islandData} />
+        ))}
       <b>TOTAL: £{total}</b>
-      <button onClick={() => window.alert("rich boy")}>CHECKOUT</button>
+      <button onClick={() => window.alert('rich boy')}>CHECKOUT</button>
       <Link href="/"> 🔙 </Link>
     </>
-  );
-}
-
-function Item({
-  islandData: { img_path: imagePath, name: islandName, region, price, area },
-}) {
-  return (
-    <div className={styles.container}>
-      <Image src={imagePath} alt={islandName} width={300} height={200} />
-      <b>{islandName}</b>
-      <small> {region}</small>
-      <p> Quantity: 2</p>
-      <p> £{price}</p>
-    </div>
   );
 }
